f685afa3c9b6b37cfa8eb3397c61b74ca713f820
jadarsh6298-tech <jadarsh6298@gmail.com>
2026-08-29 22:15:01 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+        for i,x in enumerate(nums):
+            t = target -x
+            if t in seen:
+                return [seen[t],i]
+            seen[x]=i
